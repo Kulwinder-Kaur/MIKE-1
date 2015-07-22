08a1a008239e459dd15ba03853ebe9b743f05cfa
unknown <akkulka@akkulka.fareast.corp.microsoft.com>
2015-07-22 10:35:21 +0530

    side panel for tags added

--- controllers/appctrl.js
+++ controllers/appctrl.js
@@ -1,5 +1,26 @@
 app.controller('AppCtrl',function($mdDialog,$scope){
 	$scope.name="Akshay Kulkarni";
+
+	$scope.tags = [
+		{
+			name:"C#"
+		},
+		{
+			name:"SQL"
+		},
+		{
+			name:"C"
+		},
+		{
+			name:"C++"
+		},
+		{
+			name:"ASP"
+		},
+		{
+			name:"Angular"
+		},
+	];
 });
 
 
